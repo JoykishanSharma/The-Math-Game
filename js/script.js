f72fa94e9ce68154560b9226914a6f4d18034a18
JoykishanSharma <joykishan120@gmail.com>
2020-09-20 15:43:27 +0530

    game code-walk created

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//if we click on the start/reset
+    //if we are playing
+        //reload page
+    // if we are not playing
+        //set score to 0
+        //show countdown box
+        //reduce time by 1sec in loops
+            //time left?
+                //yes --> continue
+                //no --> gameover
+        //change button to reset
+        //generate new Q&A
+
+
+//if we click on answer box
+    //if we are playing
+        //if correct?
+            //yes
+                //increase score by 1
+                //show correct box for 1sec
+                //generate new Q&A
+            //no
+                //show try again box
